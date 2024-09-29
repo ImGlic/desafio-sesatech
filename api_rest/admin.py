7c23e6a8f7f01cf7659e27f2832661da52a316f1
Victor Glicério <66135800+ImGlic@users.noreply.github.com>
2024-09-29 12:02:19 -0300

    ajustes urls and admin

--- api_rest/admin.py
+++ api_rest/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Products
 
 admin.site.register(User)
+
+admin.site.register(Products)
+
 
--- api_rest/urls.py
+++ api_rest/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 urlpatterns = [
     path('products', views.get_products, name='get_all_products'),
+
+    path('product/<int:product_id>/', views.get_product_by_id, name='get_product_by_id'), 
+    path('product_manager/', views.product_manager, name='product_manager'), 
+
+    path('sales/', views.sale_manager, name='sale_manager'), 
 ]
